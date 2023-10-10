526641f9da6abe139fa45944512269af6dbd1551
adarshmm <66505433+adarshmm@users.noreply.github.com>
2023-10-10 21:27:11 +0530

    Update HomePage.java

--- src/test/java/Outlook/pages/HomePage.java
+++ src/test/java/Outlook/pages/HomePage.java
@@ -69,3 +69,48 @@ public class SentEmailPage {
 		
 	}
 }
+package outlook.pages;
+
+public class DeleteMailPage {
+	private static void deleteEmail(WebDriver driver, String emailSubject) {
+        // Navigate to the Sent Items or Inbox, depending on your use case
+        WebElement sentItems = driver.findElement(By.xpath("//span[contains(text(), 'Sent Items')]"));
+        sentItems.click();
+
+        // Wait for the Sent Items page to load (you may need to customize the wait time)
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.titleContains("Sent Items"));
+
+        // Find and click the email with the specified subject
+        WebElement emailToDelete = driver.findElement(By.xpath("//span[contains(text(), '" + emailSubject + "')]"));
+        emailToDelete.click();
+
+        // Find and click the delete button (you may need to customize the locator)
+        WebElement deleteButton = driver.findElement(By.xpath("//div[@aria-label='Delete']"));
+        deleteButton.click();
+
+        // Optionally, you can wait for the email to disappear from the current folder
+        wait.until(ExpectedConditions.invisibilityOf(emailToDelete));
+
+        System.out.println("Email with subject '" + emailSubject + "' deleted.");
+    }
+
+    private static void checkDeletedItems(WebDriver driver, String emailSubject) {
+        // Navigate to the Deleted Items or Trash folder, depending on your use case
+        WebElement deletedItems = driver.findElement(By.xpath("//span[contains(text(), 'Deleted Items')]"));
+        deletedItems.click();
+
+        // Wait for the Deleted Items page to load (you may need to customize the wait time)
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.titleContains("Deleted Items"));
+
+        // Verify that the deleted email is present in Deleted Items
+        WebElement deletedEmail = driver.findElement(By.xpath("//span[contains(text(), '" + emailSubject + "')]"));
+
+        // Assert that the deleted email is present in Deleted Items
+        assert deletedEmail.isDisplayed() : "Email with subject '" + emailSubject + "' not found in Deleted Items.";
+
+        System.out.println("Email with subject '" + emailSubject + "' found in Deleted Items.");
+    }
+}
+}
